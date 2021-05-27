eefe81a4072776ae2b4564d0784e2c5ee7bf3304
gaotc200 <gaotc200@gmail.com>
2021-05-27 10:32:28 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rpgQTL',
+      version='0.0.1',
+      description='Regions per gene (rpg) QTL',
+      url='https://github.com/gaotc200/rpgQTL',
+      author='Jiahao Gao',
+      author_email='jiahao.gao@yale.edu',
+      license='BSD 3',
+      packages=['rpgQTL'],
+      zip_safe=False)
